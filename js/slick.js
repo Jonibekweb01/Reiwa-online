948cceae9b04b6633050301805a0a8376e114193
Jonibekweb01 <jonibekqodirovv@gmail.com>
2022-10-28 17:19:29 +0500

    Add

--- js/slick.js
+++ js/slick.js
@@ -1,15 +1,6 @@
-// $('.carousel').slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     arrows: false,
-//     fade: true,
-//     asNavFor: '.slider-nav'
-// });
-// $('.carousel').slick({
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     asNavFor: '.slider-for',
-//     dots: true,
-//     centerMode: true,
-//     focusOnSelect: true
-// });
+$('.wrapper-carousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
