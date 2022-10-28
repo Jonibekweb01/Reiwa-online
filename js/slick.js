e03efbd8dc93767edeb3770d0d2ed8d3908899b2
Jonibekweb01 <jonibekqodirovv@gmail.com>
2022-10-28 17:31:50 +0500

    Add

--- js/slick.js
+++ js/slick.js
@@ -1,6 +1,23 @@
-$('.wrapper-carousel').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-});
+
+
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        $('.wrapper-carousel').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+    } else {
+        $('.wrapper-carousel').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+    }
+}
+
+var x = window.matchMedia("(max-width: 568px)")
+myFunction(x) // Call listener function at run time
